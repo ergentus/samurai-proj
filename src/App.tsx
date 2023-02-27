d67f90aee1e2055399643116ef5bf28c7f8cb338
ergentus <ponfable999@gmail.com>
2023-02-27 07:39:37 +0300

    add new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Techologies from './Techologies';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div>
+      <Header />
+      <Techologies />
     </div>
   );
 }
---
+++ src/Techologies.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Techologies = () => {
+   return (
+      <div>
+         <ul>
+            <li>css</li>
+            <li>html</li>
+            <li>js</li>
+            <li>react</li>
+         </ul>
+      </div>
+   );
+};
+
+export default Techologies;
